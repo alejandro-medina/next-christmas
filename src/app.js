5e95f2413ca3d63154c3af8441532ad533485d0e
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2023-01-30 11:36:35 -0600

    clear interval id before another initialization

--- src/app.js
+++ src/app.js
@@ -56,6 +56,9 @@ const update = function () {
 }
 
 const startCountdown = function () {
+
+  if (intervalId) clearInterval(intervalId);
+
   intervalId = setInterval(function () {
     update();
   }, 1000)
